4dacaad13da1995c9b6e200ac6fcbcaaf92d148a
Andy Stuckey <andystuckey@mail.com>
2025-03-22 21:02:35 -0500

    Functionality for typing letters as well as 'enter' and 'backspace'

--- word-masters.js
+++ word-masters.js
@@ -5,16 +5,40 @@ const ANSWER_LENGTH = 5;
 
 async function init() {
     let currentGuess = '';
-
+    let currentRow = 0;
 
     function addLetter (letter) {
+        // add letter to the end
         if (currentGuess.length < ANSWER_LENGTH) {
             currentGuess += letter;
         } else {
+            // replace last letter
             currentGuess = currentGuess.substring(0, currentGuess.length -1) + letter;
         }
-        letters[currentGuess.length -1].innerText = letter;
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length -1].innerText = letter;
     }
+
+    async function commit() {
+        if (currentGuess.length !== ANSWER_LENGTH) {
+            // do nothing
+            return;
+        }
+
+        // TODO validate the word
+
+        // TODO do all of the marking as "correct", "close" or "wrong"
+
+        // TODO did they win or lose?
+
+        currentRow++;
+        currentGuess = '';  
+    }
+
+    function backspace() {
+        currentGuess = currentGuess.substring(0, currentGuess.length -1);
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
+    }
+
 
     document.addEventListener('keydown', function handleKeyPress (event){
         const action = event.key;
